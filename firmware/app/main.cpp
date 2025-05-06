69b521de409c25539dda9d684f87369b9d2af305
jakub-michalik <77364412+jakub-michalik@users.noreply.github.com>
2025-05-06 21:25:59 +0200

    Bring up CHIP platform: MemoryInit + InitChipStack

--- firmware/app/main.cpp
+++ firmware/app/main.cpp
@@ -7,10 +7,30 @@
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 
+#include <lib/support/CHIPMem.h>
+#include <platform/CHIPDeviceLayer.h>
+
 LOG_MODULE_REGISTER(omsl, LOG_LEVEL_INF);
+
+using namespace chip;
+using namespace chip::DeviceLayer;
 
 int main(void)
 {
     LOG_INF("openMatterSmartLock bring-up");
+
+    CHIP_ERROR err = Platform::MemoryInit();
+    if (err != CHIP_NO_ERROR) {
+        LOG_ERR("Platform::MemoryInit failed: %" CHIP_ERROR_FORMAT, err.Format());
+        return -1;
+    }
+
+    err = PlatformMgr().InitChipStack();
+    if (err != CHIP_NO_ERROR) {
+        LOG_ERR("InitChipStack failed: %" CHIP_ERROR_FORMAT, err.Format());
+        return -1;
+    }
+
+    LOG_INF("CHIP stack initialized");
     return 0;
 }
