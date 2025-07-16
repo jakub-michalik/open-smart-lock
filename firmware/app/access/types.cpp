aa47dce55d39e755ab05b0bc323af68d4d9a6623
jakub-michalik <77364412+jakub-michalik@users.noreply.github.com>
2025-07-16 22:23:03 +0200

    Add access data types: User, Credential, Schedule

---
+++ firmware/app/access/types.cpp
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2025 openMatterSmartLock contributors
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "types.h"
+
+// Reserved for future helper definitions; types are header-only for now.
---
+++ firmware/app/access/types.h
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2025 openMatterSmartLock contributors
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+#include <cstdint>
+#include <array>
+
+namespace omsl::access {
+
+constexpr size_t kMaxUsers = 10;
+constexpr size_t kMaxCredentialsPerUser = 4;
+constexpr size_t kMaxPinLength = 16;
+
+struct User {
+    uint16_t index;
+    bool active;
+    char name[32];
+};
+
+struct Credential {
+    uint16_t user_index;
+    uint16_t credential_index;
+    uint8_t type;
+    uint8_t data[kMaxPinLength];
+    uint8_t data_length;
+};
+
+struct Schedule {
+    uint16_t user_index;
+    uint8_t schedule_index;
+    uint32_t start_time;
+    uint32_t end_time;
+};
+
+}  // namespace omsl::access
